246007f9608e1a1af7e775cc0e039f83e3086e5f
kunal-thakur1 <91238202+kunal-thakur1@users.noreply.github.com>
2021-09-29 10:39:16 +0530

    Create task3.cpp

---
+++ practical_4/task3.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+namespace first{
+   int add(int x,int y)
+   {
+      return(x+y);
+   }
+}
+namespace second{
+   float add(float x,float y)
+   {  
+      return(x+y);
+   }
+}
+
+ 
+ int main()
+{
+   int a,b;
+   float q,w;
+   std::cin>>a;
+   std::cin>>b;
+   std::cout<<"sum of int ";
+   std::cout<<add(a,b)<<std::endl;
+   std::cin>>q;
+   std::cin>>w;
+   std::cout<<"sum of float ";
+   std::cout<<add(q,w);
+   return 0;
+}
+
+
