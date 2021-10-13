a1929ded69d46afba4e41f676828f03674f8a0f7
kunal-thakur1 <91238202+kunal-thakur1@users.noreply.github.com>
2021-10-13 10:55:06 +0530

    Add files via upload

---
+++ practical_07/practical07_task04.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int n=3,m=3;
+   int a[3][3] = {1,2,3,4,5,6,7,8,9};
+   int i,j;
+   for (i=0;i<3;i++)
+   {
+       for(j=0;j<3;j++)
+       {
+           a[i][j]=10-a[i][j];
+        }
+    }
+    for (i=0;i<3;i++)
+   {
+       for(j=0;j<3;j++)
+       {
+           cout<<a[i][j] << " ";
+        }
+        cout<<endl;
+    }
+    
+
+    
+    return 0;
+}
