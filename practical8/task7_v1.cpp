0e52aac0613ea1e1f22991eb128c9b782c5ae566
kunal-thakur1 <91238202+kunal-thakur1@users.noreply.github.com>
2021-10-25 14:10:10 +0530

    Add files via upload

---
+++ practical8/task7_v1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void swap(int , int);
+int main()
+{
+   int a=10;
+   int b=20;
+   cout<<"before swapping a="<<a <<"b = " <<b<<endl;
+   swap(a,b);
+   cout<<"after call by value swapping (in main) a= "<< a <<"b = "<<b<<endl;
+}
+void swap(int a,int b)
+{
+  int temp;
+  temp=a;
+  a=b;
+  b=temp;
+  cout<<"after call by value swapping (in function) a= "<< a <<"b = "<<b<<endl;
+}
